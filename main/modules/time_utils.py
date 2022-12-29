475573b6e305a0bec04cc1d8e0d06a01995e7e59
Grzegorz Krug <grzesiekk321@gmail.com>
2022-12-29 11:35:10 +0100

    Git ignore pictures. Measure time decorator

---
+++ main/modules/time_utils.py
@@ -0,0 +1,25 @@
+import time
+
+from functools import wraps
+
+
+def measure_time_decorator(fun):
+
+    @wraps(wrapped=fun)
+    def wrapper(*a, **kw):
+        fmt = ">4.1f"
+        time0 = time.perf_counter()
+        res = fun(*a, **kw)
+        t_end = time.perf_counter()
+        dur = t_end - time0
+        if dur < 1e-3:
+            timeend = f"{dur * 1000000:{fmt}} us"
+        elif dur < 1:
+            timeend = f"{dur * 1000:{fmt}} ms"
+        else:
+            timeend = f"{dur:{fmt}} s"
+        print(f"{fun.__name__} exec time: {timeend}: ")
+
+        return res
+
+    return wrapper
